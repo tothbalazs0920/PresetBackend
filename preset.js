8a99f250806b82a548e3c3bcf1d248063f8330bf
Balazs Toth <tothbalazs0920@gmail.com>
2017-06-11 15:40:55 +0200

    Use properties from Bonsai URL

--- preset.js
+++ preset.js
@@ -1,53 +1,60 @@
 var mongoose = require('mongoose');
-var mongoosastic=require("mongoosastic");
+var mongoosastic = require("mongoosastic");
 var Schema = mongoose.Schema;
+let URL = require('url');
 
 var presetSchema = new Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  name: { type:String, es_indexed:true },
-  technology: { type:String, es_indexed:true },
-  description: { type:String, es_indexed:true },
-  genre: { type : Array , "default" : [] },
+  name: { type: String, es_indexed: true },
+  technology: { type: String, es_indexed: true },
+  description: { type: String, es_indexed: true },
+  genre: { type: Array, "default": [] },
   numberOfDownLoads: Number,
-  amp: { type:String, es_indexed:true },
-  cabinet: { type:String, es_indexed:true },
-  michrophones: { type : Array , "default" : [] },
-  presetAuthor: { type:String, es_indexed:true },
+  amp: { type: String, es_indexed: true },
+  cabinet: { type: String, es_indexed: true },
+  michrophones: { type: Array, "default": [] },
+  presetAuthor: { type: String, es_indexed: true },
   lead: Boolean,
   clean: Boolean,
   rythm: Boolean,
-  author: { type:String, es_indexed:true },
-  album: { type:String, es_indexed:true },
-  songTitle: { type:String, es_indexed:true },
-  presetId: { type:String, es_indexed:true },
-  originalPerestFileName: { type:String, es_indexed:true },
+  author: { type: String, es_indexed: true },
+  album: { type: String, es_indexed: true },
+  songTitle: { type: String, es_indexed: true },
+  presetId: { type: String, es_indexed: true },
+  originalPerestFileName: { type: String, es_indexed: true },
   img: String,
-  profilePicture: { type:String, es_indexed:true },
+  profilePicture: { type: String, es_indexed: true },
   price: Number,
   currency: String,
-  audioFileId: { type:String, es_indexed:true },
-  originalAudoFileName: { type:String, es_indexed:true },
-  email: { type:String, es_indexed:true },
+  audioFileId: { type: String, es_indexed: true },
+  originalAudoFileName: { type: String, es_indexed: true },
+  email: { type: String, es_indexed: true },
 });
 
-if(process.env.NODE_ENV === 'development') {
-presetSchema.plugin(mongoosastic,{
-  hosts: process.env.ELASTICSEARCH_URL,
-  auth: process.env.ELASTICSEARCH_AUTH
-});
+if (process.env.NODE_ENV === 'development') {
+  presetSchema.plugin(mongoosastic, {
+    hosts: process.env.ELASTICSEARCH_URL,
+    auth: process.env.ELASTICSEARCH_AUTH
+  });
 } else {
+  var url = new URL(process.env.BONSAI_URL);
   console.log('conncet to Bonsai');
-  presetSchema.plugin(mongoosastic,{
-  hosts: process.env.BONSAI_URL
-});
+  console.log('url.host:', url.host);
+  console.log('url.port:', url.port);
+  console.log('url.auth:', url.auth);
+  presetSchema.plugin(mongoosastic, {
+    host: url.host,
+    port: url.port,
+    auth: url.auth
+  });
 }
 var Preset = mongoose.model('Presets', presetSchema);
 
-Preset.createMapping(function(err, mapping){
-  if(err){
+Preset.createMapping(function (err, mapping) {
+  if (err) {
     console.log('error creating mapping (you can safely ignore this)');
     console.log(err);
-  }else{
+  } else {
     console.log('mapping created!');
     console.log(mapping);
   }
