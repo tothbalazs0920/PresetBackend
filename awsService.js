9e5111323f026e1414b14714633cbb3c3f5ad5a0
Balazs Toth <tothbalazs0920@gmail.com>
2017-07-25 12:58:17 +0200

    Decouple awsService

---
+++ awsService.js
@@ -0,0 +1,40 @@
+const aws = require('aws-sdk');
+const Promise = global.Promise;
+
+aws.config.update({
+    accessKeyId: process.env.AWSAccessKeyId,
+    secretAccessKey: process.env.AWSSecretKey,
+    region: "eu-west-1"
+});
+
+const getBucketName = function (mp3) {
+    if (mp3 === 'true') {
+        return process.env.S3_BUCKET_MP3;
+    }
+    return process.env.S3_BUCKET_PRESET;
+};
+
+module.exports.getPresignedUrl = function (fileName, fileType, mp3, operation) {
+    const s3 = new aws.S3();
+    const bucket = getBucketName(mp3);
+    const s3Params = {
+        Bucket: bucket,
+        Key: fileName,
+        Expires: 600,
+        ContentType: fileType,
+        ACL: 'public-read'
+    };
+
+    return new Promise((resolve, reject) => {
+        s3.getSignedUrl(operation, s3Params, (err, data) => {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        })
+    });
+
+}
